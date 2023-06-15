8647822f84d4e03ffe132d467113425733fc6684
Shrey Kharbanda <kharbandashrey@gmail.com>
2023-06-16 00:42:48 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const fs = require("fs/promises");
 const express = require("express");
 const cors = require("cors");
-const _ = require("lodash");
-const {v4: uuid} = require("uuid");
 
 
 const app = express();
 
+app.use(express.json());
+app.use(express.cors());
+
 app.get("/youtubedata", (req, res) => {
-    const yt = {"V6mKVRU1evU":"img"}
-        res.json();
+    const yt = [["V6mKVRU1evU","img"]]
+        res.json({
+            videoId : yt[0][0],
+            videoImg : yt[0][1]
+        });
     });
 
 app.listen(3000, () => console.log("API Server is running!"));
