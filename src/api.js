2aa25da6d7f9cd25fbf87f90eb3cdcb94e187444
Shrey Kharbanda <kharbandashrey@gmail.com>
2023-06-22 20:06:11 +0530

    initial

---
+++ src/api.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const serverless = require("serverless-http");
+
+
+const app = express();
+
+// app.use(express.json());
+// app.use(express.cors());
+
+const router = express.Router();
+
+router.get("/youtubedata", (req, res) => {
+    const yt = [["V6mKVRU1evU","img"]]
+        res.json({
+            videoId : yt[0][0],
+            videoImg : yt[0][1]
+        });
+    });
+
+app.use(`/.netlify/functions/api`, router);
+
+module.exports = app;
+module.exports.handler = serverless(app);
+
+app.listen(3000, () => console.log("API Server is running!"));
