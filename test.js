64d07055e25d318c2294075c6de4becfc118c3fe
Shrey Kharbanda <kharbandashrey@gmail.com>
2023-07-07 01:30:39 +0530

    mongodb updates

--- test.js
+++ test.js
@@ -1,13 +1,24 @@
-const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
 
-// Connect to MongoDB
-mongoose.connect("mongodb+srv://sk10101:hello@cluster0.xsbzuyx.mongodb.net/?retryWrites=true&w=majority", {
+const uri = "mongodb+srv://sk10101:hello@cluster0.xsbzuyx.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+const clientPromise = client.connect();
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
+const handler = async (event) => {
+  try {
+      const database = (await clientPromise).db("Video");
+      const collection = database.collection("Video");
+      // Function logic here ...
+      const results = await collection.find({}).toArray();
+      return {
+        statusCode: 200,
+        body: JSON.stringify(results),
+    }
+  } catch (error) {
+      return { statusCode: 500, body: error.toString() }
+  }
+}
+module.exports = { handler }
